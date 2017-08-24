ee2e5b4fa76eb5f012b9663682bb52848a380797
jakedub <jacob.w.moore@gmail.com>
2017-08-24 13:18:16 -0400

    slight updates

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const app = express ();
 
 //Mongo
 const MongoClient = require("mongodb").MongoClient;
-const uri = "mongodb://localhost:27017/robots";
+const uri = "mongodb://localhost:27017/robot";
 const data = require("./data");
 
 // Mongoose
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
-mongoose.connect('mongodb://localhost:27017/robots');
+mongoose.connect('mongodb://localhost:27017/robot');
 
 
 //Mustache
@@ -19,6 +19,14 @@ app.engine("mustache", mustacheExpress());
 app.set("views", "./views")
 app.set("view engine", "mustache")
 app.use(express.static("public"))
+
+const session = require("express-session")
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+}));
 
 //NOTE  DO NOT UNCOMMENT
 // MongoClient.connect(uri)
@@ -36,7 +44,7 @@ const bcrypt = require('bcryptjs');
 
 //bcrypt user authentication
 const userSchema = new mongoose.Schema({
-  username: { type: String, unique: true, lowercase: true, required: true },
+  username: { type: Number, unique: true, lowercase: true, required: true },
   passwordHash: { type: String, required: true }
 });
 
@@ -59,10 +67,12 @@ userSchema.statics.authenticate = function(username, password, done) {
     }, function(err, user) {
         if (err) {
             done(err, false)
+            alert("Incorrect")
         } else if (user && user.authenticate(password)) {
             done(null, user)
         } else {
             done(null, false)
+            alert("Try again")
         }
     })
 };
@@ -74,8 +84,9 @@ app.get("/jobs", function(req, res){
   MongoClient.connect(uri)
     .then(function(db){
       return db.collection("users").find({job:null}).toArray(function(err, doc){
+        // console.log(doc);
         res.render("jobs", {robot:doc});
-      });
+      }); //pulls in first present but won't work with find. Need to be able to display it
       db.close();
     });
   });
@@ -96,45 +107,42 @@ app.get('/user', function (req, res) {
   res.render("user", data);
 });
 
+const list= {
+  username: 2,
+  password: "that"
+};
 
-// app.post("/", function(req,res){
-//   console.log(req.body);
-//   User.create({
-//     username: req.body.username,
-//     password: req.body.password,
-//     email: req.body.email,
-//     company: req.body.company
-//   })
-//   .then(handleSuccess)
-//   .catch(handleError);
-//   res.redirect("/completed")
-// });
+app.post("/completed", function (req,res){
+  User.create({
+    username: req.session.username,
+    password: req.session.password,
+  })
+  res.redirect("/completed");
+});
 
 app.get("/completed", function(req, res){
   return User.find()
   .then(function(users){
-  res.render("completed", {data: users})
+  res.render("completed", data);
+})
 });
 
 app.get("/login", function(req,res){
   res.render("login");
+})
+
+
+app.get('/user/:id', (req, res) => {
+  let myId = parseInt(req.params.id);
+  MongoClient.connect(uri)
+    .then((db) => {
+      let collection = db.collection('users');
+      collection.findOne({'id' : myId})
+    .then((users) => {
+      res.render("template", users);
+      db.close();
+    })})
 });
-
-
-// app.get('/user/:id', (req, res) => {
-//   let myId = parseInt(req.params.id);
-//   MongoClient.connect(uri)
-//     .then((db) => {
-//       let collection = db.collection('users');
-//       console.log('CONNECTING');
-//       collection.findOne({'id' : myId})
-//     .then((myUser) => {
-//       console.log("RENDERING...");
-//       console.log(myUser);
-//       res.render('user', myUser);
-//       db.close();
-//     })})
-// });
 
 app.listen(3000, function () {
   console.log('All hail AI');
